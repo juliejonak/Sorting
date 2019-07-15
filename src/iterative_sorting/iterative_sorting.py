18e938ff8f0bd5edef9bd339882b4c72a62e96c0
Julie Jonak <juliejonak10@gmail.com>
2019-07-15 17:43:40 -0500

    Adds count sort, fixes test error

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -28,34 +28,33 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
-    # Find highest number in arr. If any negative numbers, return err:
     hash_length = 0
+
     for i in range(0, len(arr)):
+        # If any negative numbers, returns error
         if arr[i] < 0:
             return "Error, negative numbers not allowed in Count Sort"
+        # Find highest number in arr
         if arr[i] > hash_length:
             hash_length = arr[i]
         
-    print(f'Hash length should go to {hash_length}')
     # Create hash table with maximum arr values as keys, set to 0
     hash = { i: 0 for i in range(0, hash_length+1)}
-    print(hash)
 
     # In hash, increase count of each instance of i in arr
     for i in range(0, len(arr)):
         hash[arr[i]] += 1
     
-    # Add instances in new_arr to find indices of each element in arr
+    # Adds two instance totals to find indices of each element in arr
     for i in range(1, len(hash)):
         hash[i] = hash[i] + hash[i-1]
-
-    # Go through arr to place final sorted_arr, decreasing each placed instance
+    
     sorted_arr = [None] * len(arr)
     for i in range(0, len(arr)):
+        # Places arr[i] instance into correct index of sorted_arr
         sorted_arr[hash[arr[i]]-1] = arr[i]
+        # Decreases i's index in hash
+        hash[arr[i]] = hash[arr[i]]-1
 
     return sorted_arr
 
-
-# print(count_sort([2, 5, 3, 7, 6], [0, 1, 2, 3, 4, 5, 6, 7]))
-print(count_sort([6, 3, 7, 2, 8, 1, 7, 2]))
